4a33d4e99304355bb2167b6ee137a310695b3c99
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-04-22 15:27:31 +0200

    feature(warning): add using of internal template and styles

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  selector: "app-warning-alert",
+  template: "<p>warning-alert works!</p>",
+  styles: [
+    `
+      p {
+        padding: 20px;
+        width: 200px;
+        color: red;
+        border: 2px solid red;
+      }
+    `,
+  ],
 })
-export class WarningAlertComponent {
-
-}
+export class WarningAlertComponent {}
