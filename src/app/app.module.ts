33f9f1bc2345e624c6b971cee947d8bbdba929d4
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-06-02 18:21:36 +0200

    feature(init): init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
-import { ServerComponent } from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
