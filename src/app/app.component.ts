08aa88c38dd83bd4f22c006ee94281e4f23e7a86
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-06-05 15:02:17 +0200

    feature(directives): add code to directives assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Recipes App';
+  detailsDisplayed: boolean = false;
+  logs: string[] = [];
+
+  onDisplayDetailsClick() {
+    this.detailsDisplayed = !this.detailsDisplayed;
+    this.logs.push(String(Date.now()));
+  }
+
+  getLogStyle(log: string): object {
+    const idx: number = this.logs.indexOf(log);
+    return idx >= 4 ? { backgroundColor: 'blue' } : {};
+  }
+
+  getLogClass(log: string): object {
+    const idx: number = this.logs.indexOf(log);
+    return { accent: idx >= 4 };
+  }
 }
