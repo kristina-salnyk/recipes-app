fb20c85913fbc38050dd3ce7768b4786160efb39
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-06-06 01:32:32 +0200

    feature(shopping-list): add shopping-list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Chicken', 1),
+    new Ingredient('Cheese', 200),
+  ];
 }
